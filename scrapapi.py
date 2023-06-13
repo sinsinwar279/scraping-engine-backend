930227072e83439b1aaeac3e123e7b1e895e7f8f
sinsinwar279 <129961984+sinsinwar279@users.noreply.github.com>
2023-06-13 12:22:41 +0530

    Update scrapapi.py

--- scrapapi.py
+++ scrapapi.py
@@ -169,4 +169,4 @@ def extractDataFromCSEResponse(response, data):
 
 # Run the Flask application
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, threaded=True, port=5500)
+    app.run(host='0.0.0.0', debug=True, threaded=True, port=5000)
