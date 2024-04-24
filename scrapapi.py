37f63b8795247a6efcd1093a2c3fdc91b1595738
Suraj Singh <suraj.singh@minoanexperience.com>
2024-04-24 17:23:35 +0530

    added header for westlm

--- scrapapi.py
+++ scrapapi.py
@@ -121,7 +121,7 @@ def fetchData(url, data, callCount=0):
         app.logger.info(data)
 
         if (data.get('title') == '' or data.get('title') == 'West Elm: 403 - Restricted Access'):
-            if 'westelm' in url or 'potterybarn' in url or 'rejuvenation' in url or 'williams-sonoma' in url:
+            if 'westelm' in url or 'potterybarn' in url or 'rejuvenation' in url or 'williams-sonoma' in url or 'pbteen' in url:
                 arr = url.split('/')
                 for i in range(0, len(arr)):
                     if arr[i] == 'products' and i + 1 < len(arr):
@@ -137,7 +137,7 @@ def fetchData(url, data, callCount=0):
         return data
 
     else:
-        if 'westelm' in url or 'potterybarn' in url or 'rejuvenation' in url or 'williams-sonoma' in url:
+        if 'westelm' in url or 'potterybarn' in url or 'rejuvenation' in url or 'williams-sonoma' in url or 'pbteen' in url:
             arr = url.split('/')
             for i in range(0, len(arr)):
                 if arr[i] == 'products' and i + 1 < len(arr):
